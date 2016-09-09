26e04a06aeef724d7af5db12929dfa8803e725f3
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2016-09-09 19:49:33 +0800

    Fix deploy project
    Change-Id: I7507b4fb4361d45e8aa958e3d124f5ec3fea7788
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>

--- deployment/src/main/release/bin/start.sh
+++ deployment/src/main/release/bin/start.sh
@@ -1,11 +1,11 @@
 #*******************************************************************************
-# Copyright (c) 2016, Huawei Technologies Co., Ltd.
+# Copyright 2016 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,37 +14,6 @@
 # limitations under the License.
 #*******************************************************************************
 #!/bin/bash
-
-if [ -z "$JAVA_HOME" ]
-then
-    echo "There is no JAVA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_HOME" ]
-then
-    echo "There is no CATALINA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_BASE" ]
-then
-    echo "There is no CATALINA_BASE"
-    exit 1
-fi
-
-LOG_DIR=$CATALINA_BASE/logs
-if [ ! -d "$LOG_DIR" ]; then
-  mkdir $LOG_DIR
-fi
-
-ACCESS_GRP=$OPENO_HOME/groups
-if [ ! -d "$ACCESS_GRP" ];
-then
-    mkdir $ACCESS_GRP
-fi
-
-
-$CATALINA_HOME/bin/catalina.sh start
-
-
+cd ..
+export CATALINA_BASE=$(cd `dirname $0`; pwd)
+catalina.sh start
